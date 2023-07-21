f3fa43f58609d438c3a4235f691ec3f5805a41e1
s1f102103707 <s1f102103707@inaid.org>
2023-07-21 20:06:57 +0900

    生成文字実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import { useState } from 'react';
 import styles from './index.module.css';
-
 const Home = () => {
+  const blankMaze = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
   const initialMaze = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 0, 1, 0, 1, 0, 1, 0],
@@ -13,8 +23,7 @@ const Home = () => {
     [0, 1, 0, 1, 0, 1, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
-  const [maze, setMaze] = useState(initialMaze);
-
+  const [maze, setMaze] = useState(blankMaze);
   const directions = [
     [-1, 0], // 上
     [1, 0], // 下
@@ -22,50 +31,48 @@ const Home = () => {
     [0, 1], // 右
   ];
   const [human, setHuman] = useState();
-  const onClick = (x: number, y: number) => {
-    const newMaze = JSON.parse(JSON.stringify(initialMaze)); // リセットした迷路を作成
-
-    const randomDirectionIndex = Math.floor(Math.random() * directions.length); // ランダムにdirectionsのインデックスを選択
-    const [dx, dy] = directions[randomDirectionIndex]; // 選択された方向の変位を取得
-    const newX = x + dx; // 新しいx座標
-    const newY = y + dy; // 新しいy座標
-
-    if (newX >= 0 && newX < newMaze[0].length && newY >= 0 && newY < newMaze.length) {
-      newMaze[newY][newX] = 1; // 指定された座標の0を1に変更
-      setMaze(newMaze); // 変更した迷路を反映
-    }
-  };
-
-  const onGenerateClick = () => {
-    //全ての１に対してonClick関数を呼び出す
-    maze.map((row, y) => {
-      row.map((color, x) => {
+  const onClick = () => {
+    const newMaze = JSON.parse(JSON.stringify(initialMaze));
+    newMaze.map((row: number[], y: number) => {
+      row.map((color: number, x: number) => {
         if (color === 1) {
-          onClick(x, y);
+          const randomDirectionIndex = Math.floor(Math.random() * directions.length);
+          const [dx, dy] = directions[randomDirectionIndex];
+          const newX = x + dx;
+          const newY = y + dy;
+          if (
+            newX >= 0 &&
+            newX < newMaze[0].length &&
+            newY >= 0 &&
+            newY < newMaze.length &&
+            newMaze[newY][newX] === 0
+          ) {
+            newMaze[newY][newX] = 1;
+          }
         }
       });
     });
+    setMaze(newMaze);
   };
-
   return (
-    <div className={styles.board} style={{ backgroundColor: '#000' }}>
-      <div className={styles.generate} onClick={onGenerateClick}>
+    <div className={styles.container}>
+      <div className={styles.generate} onClick={() => onClick()}>
         生成
       </div>
-      {maze.map((row, y) => (
-        <div key={y} className={styles.row}>
-          {row.map((color, x) => (
-            <div
-              key={`${y}-${x}`}
-              className={styles.cell}
-              style={{ backgroundColor: color === 0 ? '#fff' : '#000' }}
-              onClick={() => onClick(x, y)}
-            />
-          ))}
-        </div>
-      ))}
+      <div className={styles.board} style={{ backgroundColor: '#000' }}>
+        {maze.map((row, y) => (
+          <div key={y} className={styles.row}>
+            {row.map((color, x) => (
+              <div
+                key={`${y}-${x}`}
+                className={styles.cell}
+                style={{ backgroundColor: color === 0 ? '#fff' : '#000' }}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default Home;
